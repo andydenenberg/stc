f77fa2cd1f70b87bb9391d10a6fd6315f3658fb0
andydenenberg <andydenenberg@gmail.com>
2012-11-10 13:24:00 -0600

    added posts and comments

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,6 +1,6 @@
 class AttachmentsController < ApplicationController
-  # GET /attachments
-  # GET /attachments.json
+  before_filter :authenticate_user!
+
   def index
     @attachments = Attachment.all
 
